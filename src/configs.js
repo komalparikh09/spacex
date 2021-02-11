c5d91db6e2bb2270fecf7c9eaa1729f4cb7d7b5c
komalparikh09 <komalparikh09@gmail.com>
2021-02-11 17:27:24 +0530

    fixed DB issue heroku

--- src/configs.js
+++ src/configs.js
@@ -2,7 +2,7 @@
 
 const configs = {
     production: {
-        SERVER_URI: `https://warm-coast-29891.herokuapp.com:${process.env.PORT}`,
+        SERVER_URI: 'https://warm-coast-29891.herokuapp.com:3100',
     },
     development: {
         SERVER_URI: 'http://localhost:3100',

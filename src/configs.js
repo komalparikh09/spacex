de9eb9eee716fe2f8cab82c9ccdc6fc28728f824
komalparikh09 <komalparikh09@gmail.com>
2021-02-11 14:40:42 +0530

    fixed DB issue heroku

--- src/configs.js
+++ src/configs.js
@@ -1,11 +1,11 @@
 //require('dotenv').config();
 
 const configs = {
-    development: {
-        SERVER_URI: 'http://localhost:3100',
-    },
     production: {
         SERVER_URI: 'https://warm-coast-29891.herokuapp.com:3100',
+    },
+    development: {
+        SERVER_URI: 'http://localhost:3100',
     },
 };
 
